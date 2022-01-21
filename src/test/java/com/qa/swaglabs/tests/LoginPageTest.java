c26d1270fbf066660667923093f25a087c4f6d59
hiteshp6342@gmail.com <hiteshp6342@gmail.com>
2022-01-21 00:01:03 -0500

    fixes

--- src/test/java/com/qa/swaglabs/tests/LoginPageTest.java
+++ src/test/java/com/qa/swaglabs/tests/LoginPageTest.java
@@ -5,11 +5,8 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.qa.swaglabs.base.BasePage;
